0b69d56d94a4107eb36de310511150746f4f437b
cyhbyw <swust2009cyh@qq.com>
2018-03-06 07:37:04 +0800

    mix use Hibernate and SpringJDBC/MyBatis, they will share the same tx

--- chapter12/src/main/java/com/smart/mixdao/ScoreService.java
+++ chapter12/src/main/java/com/smart/mixdao/ScoreService.java
@@ -1,16 +1,16 @@
 package com.smart.mixdao;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-import org.apache.commons.dbcp.BasicDataSource;
 
 /**
  * @author 陈雄华
  * @version 1.0
  */
 @Service("scoreService")
-public class ScoreService extends BaseService{
+public class ScoreService extends BaseService {
 
     private JdbcTemplate jdbcTemplate;
 
@@ -23,6 +23,6 @@ public class ScoreService extends BaseService{
         String sql = "UPDATE t_user u SET u.score = u.score + ? WHERE user_name =?";
         jdbcTemplate.update(sql, toAdd, userName);
         BasicDataSource basicDataSource = (BasicDataSource) jdbcTemplate.getDataSource();
-        System.out.println("[scoreUserService.addScore]激活连接数量："+basicDataSource.getNumActive());
+        System.out.println("[scoreUserService.addScore]激活连接数量：" + basicDataSource.getNumActive());
     }
 }
--- chapter12/src/main/java/com/smart/mixdao/UserService.java
+++ chapter12/src/main/java/com/smart/mixdao/UserService.java
@@ -1,12 +1,13 @@
 package com.smart.mixdao;
 
-import com.smart.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.stereotype.Service;
+
+import com.smart.User;
 
 /**
  * @author 陈雄华
@@ -29,36 +30,46 @@ public class UserService extends BaseService {
     }
 
     public void logon(String userName) {
-    	System.out.println("before userService.updateLastLogonTime()..");
+        System.out.println("before userService.updateLastLogonTime()..");
+        // use Hibernate
         updateLastLogonTime(userName);
         System.out.println("end userService.updateLastLogonTime()..");
-        
+
         System.out.println("before scoreService.addScore()..");
+        // use JDBC
         scoreService.addScore(userName, 20);
         System.out.println("end scoreService.addScore()..");
     }
 
     public void updateLastLogonTime(String userName) {
-        User user = hibernateTemplate.get(User.class,userName);
+        User user = hibernateTemplate.get(User.class, userName);
         user.setLastLogonTime(System.currentTimeMillis());
         hibernateTemplate.update(user);
-        hibernateTemplate.flush();//③请看下文的分析
+        /**
+         * 重要：
+         * 因为还用了 SpringJDBC，所以此处需要强制刷新 Hibernate 的一级缓存！
+         * 否则，SpringJDBC 的数据会被 Hibernate 一级缓存的自动提交（延迟策略）覆盖掉！
+         */
+        hibernateTemplate.flush();
     }
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("com/smart/mixdao/applicationContext.xml");
         UserService service = (UserService) ctx.getBean("userService");
         JdbcTemplate jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
+        jdbcTemplate.execute("DELETE FROM t_user WHERE user_name='tom'");
         //插入一条记录，初始分数为10
-        jdbcTemplate.execute("INSERT INTO t_user(user_name,password,score,last_logon_time) VALUES('tom','123456',10,"+System.currentTimeMillis()+")");
+        String sql = "INSERT INTO t_user(user_name,password,score,last_logon_time) VALUES('tom','123456',10,"
+                + System.currentTimeMillis() + ")";
+        jdbcTemplate.execute(sql);
 
         //调用工作在无事务环境下的服务类方法,将分数添加20分
         System.out.println("before userService.logon()..");
         service.logon("tom");
         System.out.println("after userService.logon()..");
-         
+
         int score = jdbcTemplate.queryForObject("SELECT score FROM t_user WHERE user_name ='tom'", Integer.class);
-        System.out.println("score:"+score);
+        System.out.println("score:" + score);
         jdbcTemplate.execute("DELETE FROM t_user WHERE user_name='tom'");
     }
 }
