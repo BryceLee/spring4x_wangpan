7f3f30fe72439a7288f058a7346f17b6e2edc1c5
cyhbyw <swust2009cyh@qq.com>
2018-04-30 15:42:22 +0800

    thread local

--- chapter13/src/main/java/SimpleThreadLocal.java
+++ chapter13/src/main/java/SimpleThreadLocal.java
@@ -3,24 +3,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SimpleThreadLocal {
-	private Map valueMap = Collections.synchronizedMap(new HashMap());
-	public void set(Object newValue) {
-		valueMap.put(Thread.currentThread(), newValue);
-	}
-	public Object get() {
-		Thread currentThread = Thread.currentThread();
-		Object o = valueMap.get(currentThread);
-		if (o == null && !valueMap.containsKey(currentThread)) {
-			o = initialValue();
-			valueMap.put(currentThread, o);
-		}
-		return o;
-	}
 
-	public void remove() {
-		valueMap.remove(Thread.currentThread());
-	}
-	public Object initialValue() {
-		return null;
-	}
+    private Map valueMap = Collections.synchronizedMap(new HashMap());
+
+    public void set(Object newValue) {
+        valueMap.put(Thread.currentThread(), newValue);
+    }
+
+    public Object get() {
+        Thread currentThread = Thread.currentThread();
+        Object o = valueMap.get(currentThread);
+        if (o == null && !valueMap.containsKey(currentThread)) {
+            o = initialValue();
+            valueMap.put(currentThread, o);
+        }
+        return o;
+    }
+
+    public void remove() {
+        valueMap.remove(Thread.currentThread());
+    }
+
+    public Object initialValue() {
+        return null;
+    }
 }
