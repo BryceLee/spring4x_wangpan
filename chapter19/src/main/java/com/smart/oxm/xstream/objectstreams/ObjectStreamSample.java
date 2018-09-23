41a67a292c684795a26c687b9e83b2b22adeb657
cyhbyw <swust2009cyh@qq.com>
2018-09-23 17:07:24 +0800

    XStream 流化对象

--- chapter19/src/main/java/com/smart/oxm/xstream/objectstreams/ObjectStreamSample.java
+++ chapter19/src/main/java/com/smart/oxm/xstream/objectstreams/ObjectStreamSample.java
@@ -10,64 +10,58 @@ import java.util.Date;
 import com.smart.domain.LoginLog;
 import com.smart.domain.User;
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.HierarchicalStreamReader;
-import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
-import com.thoughtworks.xstream.io.xml.*;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
 
 public class ObjectStreamSample {
-	private static  XStream xstream;
-	static{
-		xstream = new XStream();
-	}
-	//XML转化为JAVA对象
-	public User xmlToObject()throws Exception{
-		//1.我们需要通过对象流进行输入操作，需要FileReader和BufferedReader
-		FileReader fr=new FileReader("D:\\masterSpring\\chapter14\\out\\ ObjectStreamSample.xml");
-		BufferedReader br=new BufferedReader(fr);
-		//2.创建对象输入流
-		ObjectInputStream input = xstream.createObjectInputStream(br);
-		//3.创建对象，通过ObjectInputStream中的readObject()方法将对象从XML文件中读取出来。
-		User user=(User)input.readObject();
-		return user;
- 
-	}
-	// 初始化转换对象
-	public static User getUser() {
-		LoginLog log1 = new LoginLog();
-		LoginLog log2 = new LoginLog();
-		log1.setIp("192.168.1.91");
-		log1.setLoginDate(new Date());
-		log2.setIp("192.168.1.92");
-		log2.setLoginDate(new Date());
+
+    private static XStream xstream;
+
+    static {
+        xstream = new XStream();
+    }
+
+    public User xmlToObject() throws Exception {
+        FileReader fileReader = new FileReader("chapter19\\out\\ObjectStreamSample.xml");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        ObjectInputStream objectInputStream = xstream.createObjectInputStream(bufferedReader);
+        User user = (User) objectInputStream.readObject();
+        return user;
+    }
+
+    public static User getUser() {
+        LoginLog log1 = new LoginLog();
+        LoginLog log2 = new LoginLog();
+        log1.setIp("192.168.1.91");
+        log1.setLoginDate(new Date());
+        log2.setIp("192.168.1.92");
+        log2.setLoginDate(new Date());
         User user = new User();
         user.setUserId(1);
         user.setUserName("xstream");
         user.addLoginLog(log1);
         user.addLoginLog(log2);
         return user;
-	}
-	//JAVA对象转化为XML
-	public void objectToXml()throws Exception{
-		User user = getUser();
-		PrintWriter pw=new PrintWriter("D:\\masterSpring\\chapter14\\out\\ObjectStreamSample.xml");
-		
-		//创建一个PrintWriter对象，用于输出。
-		PrettyPrintWriter ppw=new PrettyPrintWriter(pw);
-		ObjectOutputStream out = xstream.createObjectOutputStream(ppw);
-		out.writeObject(user);
-		out.close();
-	}
-	
-	public static void main(String[] args)throws Exception {
-		ObjectStreamSample converter = new ObjectStreamSample();
-	    converter.objectToXml();
-		User u = converter.xmlToObject();
-		for (LoginLog log : u.getLogs()) {
-			if (log != null) {
-				System.out.println("访问IP: " + log.getIp());
-				System.out.println("访问时间: " + log.getLoginDate());
-			}
-		}
+    }
 
-	}
+    public void objectToXml() throws Exception {
+        User user = getUser();
+        PrintWriter printWriter = new PrintWriter("chapter19\\out\\ObjectStreamSample.xml");
+        PrettyPrintWriter prettyPrintWriter = new PrettyPrintWriter(printWriter);
+        ObjectOutputStream objectOutputStream = xstream.createObjectOutputStream(prettyPrintWriter);
+        objectOutputStream.writeObject(user);
+        objectOutputStream.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        ObjectStreamSample converter = new ObjectStreamSample();
+        converter.objectToXml();
+        User u = converter.xmlToObject();
+        for (LoginLog log : u.getLogs()) {
+            if (log != null) {
+                System.out.println("访问IP: " + log.getIp());
+                System.out.println("访问时间: " + log.getLoginDate());
+            }
+        }
+
+    }
 }
